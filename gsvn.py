8fdd134036e1ba4f46de48f6fa0d744aa1f3c3d9
panchangyun <panchangyun@tuniu.com>
2015-08-05 16:01:59 +0800

    add run to module

--- gsvn.py
+++ gsvn.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
-import pygsvn.cmd
-
-def main(argv):
-    if len(argv) <= 1:
-        cmd = 'help'
-        args = []
-    else:
-        cmd = argv[1]
-        args = argv[2:]
-
-    cmd_executor = pygsvn.cmd.get_cmd(cmd)
-    if cmd_executor == None:
-        print "Error: Unknown cmd '%s'!" % cmd
-        return 1
-
-    return cmd_executor.execute(*args)
-
+import pygsvn
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    pygsvn.run()
